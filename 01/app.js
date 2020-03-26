ee531b2a8f02bd5ee08eea22e56ac842324a014a
kom-1010 <kom-1010@naver.com>
2020-03-26 09:01:24 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,70 @@
+console.log('app.js');
+
+//var name = '강순범';
+//var Name = '강순범';
+//console.log(name);
+//console.log(Name);
+
+//console.log(1);
+//console.log(2);
+
+// var count = 1;
+// console.log(count);
+
+// var isMan = true;//bool 
+// var isman = 'true'//String
+
+// console.log(isMan);
+// console.log(isman);
+
+// function log(str){
+    // console.log(str);
+// };
+
+// log("hello");
+
+// var image = {
+    // width: 100,
+    // height: 100,
+    // name: '1.png',
+// };
+
+// console.log(image);
+
+// var image1 = [1, 2, 3, 4, 5, 6, 'a', {}];
+// image1[0] = 'a';
+
+// var data = null;
+// var data1 = '';
+// var data2;
+
+// console.log(data, data1, data2);
+
+// console.log(1+2);
+
+// console.log('a' + 'b');
+// console.log(1 + '4');
+
+// var count = 1;
+// var number = '2';
+// console.log(count + number);
+
+// console.log(1==1);
+// console.log(1==2);
+// console.log(''=='0');
+// console.log(0 === '', 0 == '');
+
+// if(true){
+    // console.log(1);
+// }
+
+// if(false){
+    // console.log(2);
+// }
+
+console.log(typeof 1);
+console.log(typeof '');
+console.log(typeof {});
+console.log(typeof []);
+
+
