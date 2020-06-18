91da6ba97c034896ebe16ffe464db01d53542867
kom-1010 <kom-1010@naver.com>
2020-06-18 22:49:29 +0900

    react로 todo 개발

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Headers from './Header';
 
 function App() {
-  return <div className="App">hello</div>;
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleChange(event) {
+    setTodo(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newTodos = [...todos];
+    newTodos.push(todo);
+    setTodos(newTodos);
+    setTodo('');
+  }
+
+  return (
+    <div className="container">
+      <header>
+        <Headers />
+        <form className="new-task" id="input-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="text"
+            placeholder="할일을 입력하세요"
+            value={todo}
+            onChange={handleChange}
+          />
+        </form>
+      </header>
+      <div id="result">
+        {todos.map((todo) => {
+          return <li>{todo}</li>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
